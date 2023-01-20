4644bea9e53f4a4040f9e7c8cea799847f989cd4
tvk777 <klimenko.tv7@gmail.com>
2023-01-20 18:08:21 +0200

    solve day1 part2

--- 2015/day1/index.js
+++ 2015/day1/index.js
@@ -3,5 +3,18 @@ const example = 'example.txt';
 const fs = require('fs');
 const input = fs.readFileSync(data, 'utf-8').split('');
 //console.log(input);
-const resultPart1 = input.reduce((acc, curr) => curr==='(' ? acc+1 : acc-1, 0);
+const resultPart1 = input.reduce((acc, curr) => curr === '(' ? acc + 1 : acc - 1, 0);
 console.log(resultPart1);
+
+let resultPart2 = 0;
+let floor = 0;
+for (let i = 0; i < input.length; i++) {
+    if (input[i] === '(') floor += 1;
+    if (input[i] === ')') floor -= 1;
+    if (floor === -1) {
+        resultPart2 = i + 1;
+        break;
+    }
+}
+
+console.log(resultPart2);
