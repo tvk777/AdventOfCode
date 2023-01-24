fc9213756904a92765cc385b1f057efb030c7e0c
tvk777 <klimenko.tv7@gmail.com>
2023-01-24 14:37:37 +0200

    common solution for part and part2

--- 2015/day1/index.js
+++ 2015/day1/index.js
@@ -18,3 +18,17 @@ for (let i = 0; i < input.length; i++) {
 }
 
 console.log(resultPart2);
+
+let base = null;
+const result = input.reduce((acc, curr, index) => {
+    if(acc===-1 && !base) {
+        base = index;
+    }
+    if(curr === '(') return acc + 1; 
+    if(curr === ')') return acc - 1;
+    
+}, 0);
+
+console.log('part1', result);
+console.log('part2', base);
+
