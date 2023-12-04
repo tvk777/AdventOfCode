69df2e2594f4ed124f304f2f169221d19b9813c6
tvk1709 <klimenko.tv7@gmail.com>
2023-12-04 15:30:47 +0200

    add solution day2

--- 2023/day2/index.ts
+++ 2023/day2/index.ts
@@ -1,36 +1,79 @@
 // to run file use: ts-node index.ts
-import { example, data } from './data';
+import { example, data } from "./data";
 
-const transformData = (data: string): string[][][] =>
-  data.split(/\n/).map((i) => i.split(';').map((i) => i.split(',').map((i) => i.trim())));
+const transformData = (data: string): string[][] =>
+  data.split(/\n/).map((i) => i.split(";"));
 
-const testInput: string[][][] = transformData(example);
-const input: string[][][] = transformData(data);
+const sumNumbers = (a:number, b:number):number => a+b;  
 
-console.log(testInput);
+const testInput: string[][] = transformData(example);
+const input: string[][] = transformData(data);
 
-//const regExB: RegExp = /\d+\sblue/;
-console.log('6 blue'.indexOf('blue'));
-const MAX_BLUE = 14;
-const MAX_GREEN = 13;
 const MAX_RED = 12;
+const MAX_GREEN = 13;
+const MAX_BLUE = 14;
 
-const solutionPart1 = (input: string[][][]): number[] => {
+const regExRed: RegExp = /\d+\sred/;
+const regExGreen: RegExp = /\d+\sgreen/;
+const regExBlue: RegExp = /\d+\sblue/;
+
+const solutionPart1 = (input: string[][]): number => {
   const possibleGames: number[] = [];
   input.forEach((game, index) => {
     let isPossible = true;
 
     game.forEach((round) => {
-      round.forEach((color) => {
-        if(color.indexOf('blue')>0) {
-            
-        }
-      });
+      const blue = round.match(regExBlue);
+      const green = round.match(regExGreen);
+      const red = round.match(regExRed);
+
+      const matchResultBlue = blue && blue[0].match(/\d+/);
+      const matchResultGreen = green && green[0].match(/\d+/);
+      const matchResultRed = red && red[0].match(/\d+/);
+
+      const blueCount: number = matchResultBlue ? +matchResultBlue : 0;
+      const greenCount: number = matchResultGreen ? +matchResultGreen : 0;
+      const redCount: number = matchResultRed ? +matchResultRed : 0;
+
+      if (blueCount > MAX_BLUE || greenCount > MAX_GREEN || redCount > MAX_RED)
+        isPossible = false;
     });
 
     if (isPossible) possibleGames.push(index + 1);
   });
-  return possibleGames;
+  return possibleGames.reduce(sumNumbers, 0);
 };
 
-console.log(solutionPart1(testInput));
+//console.log(solutionPart1(input));
+
+const solutionPart2 = (input: string[][]): any => {
+  const games: number[] = [];
+
+  input.forEach((game) => {
+    let maxRed: number = 0;
+    let maxGreen: number = 0;
+    let maxBlue: number = 0;
+
+    game.forEach((round) => {
+      const red = round.match(regExRed);
+      const green = round.match(regExGreen);
+      const blue = round.match(regExBlue);
+
+      const matchResultRed = red && red[0].match(/\d+/);
+      const matchResultGreen = green && green[0].match(/\d+/);
+      const matchResultBlue = blue && blue[0].match(/\d+/);
+
+      const redCount: number = matchResultRed ? +matchResultRed : 0;
+      const greenCount: number = matchResultGreen ? +matchResultGreen : 0;
+      const blueCount: number = matchResultBlue ? +matchResultBlue : 0;
+
+      if (redCount > maxRed) maxRed = redCount;
+      if (greenCount > maxGreen) maxGreen = greenCount;
+      if (blueCount > maxBlue) maxBlue = blueCount;
+    });
+    games.push(maxRed * maxGreen * maxBlue);
+  });
+  return games.reduce(sumNumbers, 0);
+};
+
+console.log(solutionPart2(input));
